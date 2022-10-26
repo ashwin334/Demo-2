6a1fb5ea94d2c2b6bf220fd4beef391ec57316f0
ashwin333 <ashwinpatil832@gmail.com>
2022-10-26 13:03:28 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstDemo
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = 2022;
+            string month = "Sept";
+            // using + sign
+          Console.WriteLine("Current year is "+year+" &  month  is "+month);
+
+           //placeholder {0}
+
+          Console.WriteLine("Current year is {0} & month is {1}",year,month);
+
+         //interpolation --> $ sign
+
+          Console.WriteLine($"Current year is {year} & month is (month)");
+        }
+    }
+}
